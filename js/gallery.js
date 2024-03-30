6c91a4ad3bc5564e3115051112652e770ae7378b
Yevhenii Sarancha <yevhenii.sarancha@outlook.com>
2024-03-30 20:23:56 +0200

    lts version

---
+++ js/gallery.js
@@ -0,0 +1,31 @@
+import { images } from "/js/images-array.js";
+
+const galleryMarkup = images
+  .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img
+          class="gallery-image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+  `;
+  })
+  .join("");
+
+const openLightBox = (event) => {
+  if (event.target.nodeName !== "IMG") return;
+
+  basicLightbox
+    .create(`<img src="${event.target.getAttribute("data-source")}"/>`)
+    .show();
+};
+
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryContainer.addEventListener("click", openLightBox);
